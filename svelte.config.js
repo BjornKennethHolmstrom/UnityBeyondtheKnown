13d0ae902a7378a87476ddb51cb6e3bd64b8b42a
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2025-01-08 00:14:03 +0100

    Prepare for launch on GitHub pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,20 +2,29 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+  extensions: ['.svelte', '.svx'],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html', // Fallback for SPA behavior
+      strict: false // Disable strict mode
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/UnityBeyondtheKnown' : ''
+    },
+    prerender: {
+      entries: ['*'] // Pre-render all routes
+    }
+  },
 
-	extensions: ['.svelte', '.svx']
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      extension: '.svx'
+    })
+  ]
 };
 
 export default config;
+
