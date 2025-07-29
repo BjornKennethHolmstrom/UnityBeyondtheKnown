712f7bbbd3311eaf6b407dfc5f0183bcd7c06816
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2025-07-29 14:54:22 +0200

    Fix invisible language selector

--- src/lib/components/LanguageSelector.svelte
+++ src/lib/components/LanguageSelector.svelte
@@ -54,7 +54,7 @@
 
   {#if isOpen}
     <div
-      class="absolute right-0 sm:right-0 mt-2 py-2 w-48 bg-slate-800 rounded-lg shadow-xl z-50 transform -translate-x-full sm:translate-x-0"
+      class="absolute right-0 mt-2 py-2 w-48 bg-slate-800 rounded-lg shadow-xl z-50"
     >
       {#each languages as language}
         <button
